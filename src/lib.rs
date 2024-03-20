cc0e15c1b0027790cb8af3a332e374859763ac7f
1Kill2Steal <ivankirilov640@gmail.com>
2024-03-20 20:59:05 +0200

    Further documentation improvements. Decided to not update the version because there's no new features added.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,37 @@
+//! LeetCode Trees's purpose is making LeetCode problems easier to work around with via some simple
+//! library exports that allow for a smoother development experience by allowing you to use your
+//! primary IDE rather than the LeetCode website to solve the problems.
+//!
+//! These are the two ways to can add this library to your project:
+//! Number 1 - Via the Cargo.toml file by adding this;
+//!
+//! ```toml
+//! [dependencies]
+//! leetcode-trees-rs = "0.1"
+//! ```
+//!
+//! Number 2 - Via running the following command in your terminal:
+//!
+//! ```sh
+//! cargo add leetcode-trees-rs
+//! ```
+//!
+//! The usage is very simple. These are the primary components:
+//! [`prelude`]: crate::prelude
+//! [`TreeNode`]: crate::utils::TreeNode
+//!
+//!
+//! Additionally, via the support of the cargo make library you can comfortably run your LeetCode
+//! tests by running:
+//!
+//! ```sh
+//! cargo make leetcode_problem_number
+//! ```
+//!
+//! Assuming there's a LeetCode submittion for it. All the solutions are managed on the GitHub page
+//! of this libray.
+//! [solutions]: https://github.com/1Kill2Steal/leetcode-trees-rs/tree/main/solutions
+
 pub use crate::prelude::*;
 
 pub mod error;
