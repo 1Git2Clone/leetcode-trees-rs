34a65d337d3d8aaf7c744609fe0f86d7d0ce4fc8
1Kill2Steal <ivankirilov640@gmail.com>
2024-05-12 19:22:23 +0300

    Re-exports of the tree node and linked list

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,4 +1,7 @@
+pub mod linked_lists;
 pub mod trees;
 
+/// A re-export for the `linked_lists::ListNode` struct.
+pub use linked_lists::ListNode;
 /// A re-export for the `trees::TreeNode` struct.
 pub use trees::TreeNode;
