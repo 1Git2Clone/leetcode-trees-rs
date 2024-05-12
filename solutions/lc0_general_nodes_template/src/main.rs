8f7cd4d7bf5ed8a8cdd807045fcc519f3a510122
1Kill2Steal <ivankirilov640@gmail.com>
2024-05-12 22:51:43 +0300

    Updated the example to include ListNode too

--- solutions/lc0_general_nodes_template/src/main.rs
+++ solutions/lc0_general_nodes_template/src/main.rs
@@ -3,7 +3,11 @@
 #![allow(dead_code)]
 
 /// The simple library that contains the TreeNode schema.
-use leetcode_trees_rs::{prelude::*, utils::TreeNode};
+use leetcode_trees_rs::{
+    prelude::*,
+    symmetric_tree,
+    utils::{list_node, ListNode, TreeNode},
+};
 
 /// This empty struct needs to be declared to match LeetCode's struct implementation (method)
 /// scoping.
@@ -14,6 +18,7 @@ struct Solution {}
 use std::{cell::RefCell, rc::Rc};
 /// This is a personal preference. Feel free to change that part around if you wish.
 type TreeNodeElement = Option<Rc<RefCell<TreeNode>>>;
+type ListNodeElement = Option<Box<ListNode>>;
 
 impl Solution {
     pub fn your_leetcode_fn() {}
@@ -23,7 +28,7 @@ impl Solution {
 
 // #region Test NodeTree functions
 
-fn test_node_root_with_2_branches() -> TreeNode {
+fn test_tree_node_root_with_2_branches() -> TreeNode {
     let right_child_two = TreeNode::new(6);
     let right_child_one = TreeNode {
         val: 6,
@@ -41,6 +46,32 @@ fn test_node_root_with_2_branches() -> TreeNode {
         left: Some(Rc::from(RefCell::from(left_child_one))),
         right: Some(Rc::from(RefCell::from(right_child_one))),
     }
+}
+
+/// The list_node! macro invocation is equivalent to this:
+/// ```rust
+/// use leetcode_trees_rs::utils::ListNode;
+/// use std::boxed::Box;
+///
+/// ListNode {
+///     val: 1,
+///     next: Some(Box::new(ListNode {
+///         val: 2,
+///         next: Some(Box::new(ListNode {
+///             val: 3,
+///             next: Some(Box::new(ListNode::new(4)))
+///         }))
+///     }))
+/// }
+/// ```
+/// It's very visible that the boilerplate is a bit painful to deal with, that's why the macro is
+/// re-exported in the `leetcode_trees_rs::utils` module.
+fn test_list_node() -> ListNode {
+    list_node!(1, 2, 3, 4)
+}
+
+fn symmetric_tree_node() -> TreeNode {
+    symmetric_tree!(1, 2, 3, 4)
 }
 
 // #endregion
