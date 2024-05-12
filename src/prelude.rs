fa06b74e0f0e92db9e61749eece34a12652511c4
1Kill2Steal <ivankirilov640@gmail.com>
2024-05-12 19:20:55 +0300

    Changed the error type to box dyn error

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,6 +1,4 @@
-use crate::error::Error;
-
-pub type Result<T> = core::result::Result<T, Error>;
+pub type Result<T> = core::result::Result<T, Box<dyn std::error::Error>>;
 
 /// Common DSA Re-exports
 pub use std::collections::{HashMap, HashSet, VecDeque};
